88706bbff286b81e01c698bab89a49cee25ed666
craig-dsilva <craig.s.dsilva@gmail.com>
2023-09-08 14:21:36 +0100

    add titlebar icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Weather App',
-  description: 'React weather app',
-}
+  title: "Weather App",
+  description: "React weather app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="icon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
